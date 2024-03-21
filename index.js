7303349af674bb5840fa41ab7e6b03b7ac7bfb06
Arda Koca <143559366+Byte-001@users.noreply.github.com>
2024-03-21 14:15:22 +0300

    add: HTML element selections

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//* selecting html elements
+const resetButton = document.querySelector('.again');
+const numberDiv = document.querySelector('.number');
+const guessInput = document.querySelector('.guess');
+const checkButton = document.querySelector('.check');
+const msg = document.querySelector('.message');
+const highScore = document.querySelector('.high-score');
+
