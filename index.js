b6e5da354dff21817689ebbb8107aec5fe0304ae
Arda Koca <143559366+Byte-001@users.noreply.github.com>
2024-03-22 23:49:45 +0300

    add: functions for reset,check and random number

--- index.js
+++ index.js
@@ -2,9 +2,52 @@
 
 //* selecting html elements
 const resetButton = document.querySelector('.again');
-const numberDiv = document.querySelector('.number');
-const guessInput = document.querySelector('.guess');
+let numberDiv = document.querySelector('.number');
+let guessInput = document.querySelector('.guess');
 const checkButton = document.querySelector('.check');
-const msg = document.querySelector('.message');
-const highScore = document.querySelector('.high-score');
+let msg = document.querySelector('.message');
+let highScore = document.querySelector('.high-score');
+let score  = document.querySelector('.score');
 
+//* default number
+numberDiv.textContent = 0;
+
+//* functions 
+checkButton.addEventListener('click', checkNumbers);
+
+function changeMsg(message){
+    msg.textContent = message;
+}
+
+function generateRandomNumber(){
+    let randomNumber = Math.floor(Math.random() * 21) + 1;
+    return randomNumber;
+}
+
+function checkNumbers(){
+    let inputNumber = guessInput.value;
+    let rndNumber = generateRandomNumber(); 
+
+    if(inputNumber < rndNumber){
+        changeMsg('📉 Too small!');
+
+    }
+    if(inputNumber > rndNumber){
+        changeMsg('📈 Too high!');
+    }
+    if(inputNumber === rndNumber){
+         numberDiv.textContent = String(rndNumber);
+         document.body.style.backgroundColor = '#C1F2B0';
+         changeMsg('✅ Congratss! u guessed right');
+    }
+}
+
+resetButton.addEventListener('click',resetAll);
+
+function resetAll(){
+    changeMsg('');
+    numberDiv.textContent = 0;
+    score.textContent = 0;
+    highScore.textContent = 0;
+    document.body.style.backgroundColor = '#222';
+}
